7adb0dd96b4030a99c47c563c82590626f009a69
Sya077 <167753878+Sya077@users.noreply.github.com>
2024-07-25 15:49:30 +0700

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import InstagramFeed from "./components/InstagramFeed";
 import InstagramNewFeed from "./components/InstagramNewFeed";
